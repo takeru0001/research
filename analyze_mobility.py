87ef0eebb705ff9569bd3580a2d36ce62e43508b
takeru0001 <takeru-n@weseek.co.jp>
2021-01-25 16:13:52 +0900

    update analyze mobility

--- analyze_mobility.py
+++ analyze_mobility.py
@@ -9,8 +9,8 @@ with open(filelist,"r") as f:
     filepath = "./cabspottingdata/" + "new_" + line[1] + ".txt"
     infilename_list.append(filepath)
 
-if not os.path.exists("./cabspottingdata3"):
-  os.makedirs("cabspottingdata3")
+if not os.path.exists("./cabspottingdata_returner_airport"):
+  os.makedirs("cabspottingdata_returner_airport")
 
 new_infilename_list = []
 
@@ -19,6 +19,7 @@ new_infilename_list = []
 for infilename in infilename_list:
   infile = open(infilename, "r")
   airport_count = 0
+  down_town_count = 0
   for line in infile:
     data_tmp_list = line.split(" ")
     unixtime = data_tmp_list[3].replace('\r\n','')
@@ -26,15 +27,18 @@ for infilename in infilename_list:
     latitude  = float(data_tmp_list[0])
     if (37.650508 > latitude > 37.592989 and -122.446575< longitude < -122.348721):
       airport_count += 1
-  if(airport_count >= 4000):
+    # if (37.708441 > latitude > 37.574652 and -122.525680< longitude < -122.337455):
+    #   down_town_count += 1
+  # print(airport_count)
+  if(airport_count > 3500):
     convert_file_name = infilename.split('/')
-    newFileName = "./cabspottingdata3/" + convert_file_name[2]
+    newFileName = "./cabspottingdata_returner_airport/" + convert_file_name[2]
     new_infilename_list.append(newFileName)
     shutil.copy2(infilename, newFileName)
 
 
   infile.close()
 
-with open("./cabspottingdata3/_cabs.txt", "w") as f:
+with open("./cabspottingdata_returner_airport/_cabs.txt", "w") as f:
   for file_name in new_infilename_list:
     f.write(str(file_name) + "\n")
